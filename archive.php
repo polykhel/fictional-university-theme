29afbd225d2011e990ed8048d2814804d181b81d
Michael Pollente <pollentem@gmail.com>
2022-06-03 19:24:57 +0800

    Add archive and front page

--- archive.php
+++ archive.php
@@ -6,9 +6,9 @@ get_header(); ?>
         <div class="page-banner__bg-image"
              style="background-image: url(<?php echo get_theme_file_uri( 'images/ocean.jpg' ) ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">Welcome to our blog!</h1>
+            <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
             <div class="page-banner__intro">
-                <p>Keep up with our latest news.</p>
+                <p><?php the_archive_description(); ?></p>
             </div>
         </div>
     </div>
@@ -18,13 +18,15 @@ get_header(); ?>
 		while ( have_posts() ) {
 			the_post(); ?>
             <div class="post-item">
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+                <h2 class="headline headline--medium headline--post-title"><a
+                            href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
                 <div class="metabox">
-                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', ') ?></p>
+                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time( 'n.j.y' ); ?>
+                        in <?php echo get_the_category_list( ', ' ) ?></p>
                 </div>
 
                 <div class="generic-content">
-                    <?php the_excerpt(); ?>
+					<?php the_excerpt(); ?>
                     <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
                 </div>
             </div>
