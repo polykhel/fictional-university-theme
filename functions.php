062e4126521eda19708dcc1e7890707864de5c89
Michael Pollente <pollentem@gmail.com>
2022-06-04 23:31:35 +0800

    Add program custom post type

--- functions.php
+++ functions.php
@@ -16,7 +16,13 @@ add_action( 'wp_enqueue_scripts', 'university_files' );
 add_action( 'after_setup_theme', 'university_features' );
 
 function university_adjust_queries( $query ) {
-	if ( ! is_admin() and is_post_type_archive( 'event' ) and $query->is_main_query() ) {
+	if ( ! is_admin() and is_post_type_archive( 'program' ) and is_main_query() ) {
+		$query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
+		$query->set( 'posts_per_page', -1 );
+	}
+
+	if ( ! is_admin() and is_post_type_archive( 'event' ) and is_main_query() ) {
 		$today = date( 'Ymd' );
 		$query->set( 'meta_key', 'event_date' );
 		$query->set( 'orderby', 'meta_value_num' );
